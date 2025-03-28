edabbc2caf3cb41d7eee7c7b5936b7bb8c701ebb
beni0916 <lee0939037779@gmail.com>
2025-03-28 13:30:13 +0800

    update

--- Posit_remhalf.cpp
+++ Posit_remhalf.cpp
@@ -8,35 +8,20 @@ using namespace std;
 	__int32_t Posit_remhalf(x)
 	Posit64 x, y[];
 #endif
-{   
-    Posit64 pi_over2{0.5};
-    __int32_t n = (int)x * 2;
-    Posit64 nx = x - pi_over2 * n;
+{
+    static const Posit64 PI_high = 3.14159265358979323846264338327950288419716939937510;
+    static const Posit64 PI_lo = 1.224646799147353207e-16;
+    static const Posit64 half = 0.5;
 
-    while(nx > pi_over2){
-        nx = nx - pi_over2;
-        n++;
-    }
+    __int32_t n = (int)(x * 2); /* 快速計算 n = x / 0.5 */
+    Posit64 high_correction = n * half;
+    Posit64 low_correction = n * PI_lo;
 
-    while(nx < -pi_over2){
-        nx = nx + pi_over2;
-        n--;
-    }
+    Posit64 nx = x - high_correction; /* 減去高位補償 */
+    nx = nx - low_correction; /* 減去低位補償 */
 
-    if(nx < 0){
-        nx = nx + pi_over2;
-        n--;
-    }
-
-    y[0] = nx * PI;
-    y[1] = 0;
-
+    y[0] = nx * PI_high; /* 高精度結果 */
+    y[1] = nx * PI_lo;   /* 保留低位 */
     return n;
 }
 
-// int main(){
-//     Posit64 value{-123};
-//     Posit64 y[2] = {0, 0};
-//     cout<< Posit_rempio2(value, y)<<endl;
-//     cout<< y[0] <<endl;
-// }
