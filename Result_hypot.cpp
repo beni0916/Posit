5f433e849b6b9bce75d8d85601c3f387c41f3b55
ZhaoL <61046297+zhao-ng327@users.noreply.github.com>
2025-10-02 15:41:11 +0800

    Update Result_hypot.cpp

--- Result_hypot.cpp
+++ Result_hypot.cpp
@@ -40,7 +40,7 @@ int main() {
     random_device rd;
     mt19937 generator(rd());   
     mpfr_t result;
-    double input;
+    double input,base;
     string num1, num2, num3;
     vector<double> IEEE, POS;
     ofs.open("output/Posit_hypot.csv");
